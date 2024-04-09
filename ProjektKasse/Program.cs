d336d4ecad36c4eb12663b30f203da4ea94aabd0
Talhak06 <111420547+Talhak06@users.noreply.github.com>
2024-04-09 12:06:20 +0200

    Update Program.cs

--- ProjektKasse/Program.cs
+++ ProjektKasse/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjektKasse.Daten;
+using ProjektKasse;
